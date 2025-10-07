1f1f72e634407f86397335f5b269cdc1ddc613da
Sazgr <kyleyzhang@gmail.com>
2025-10-06 20:11:21 -0400

    Render static UI elements

--- menuscreen.cpp
+++ menuscreen.cpp
@@ -14,11 +14,19 @@ MenuAction MenuScreen::render(int screenWidth, int screenHeight, int offsetX, in
     const Color BACKGROUND_COLOR = hexToColor("#2A2D32", 1.0f);
     const Color PRIMARY_COLOR_100 = hexToColor("#09B58A", 1.0f);
     const Color PRIMARY_COLOR_10 = hexToColor("#09B58A", 0.1f);
-    const Color ACCENT_LIGHT_COLOR_100 = hexToColor("#E4F4F4", 1.0f);
-    const Color ACCENT_LIGHT_COLOR_80 = hexToColor("#E4F4F4", 0.8f);
-    const Color ACCENT_LIGHT_COLOR_40 = hexToColor("#E4F4F4", 0.4f);
-    const Color ACCENT_MEDIUM_COLOR = hexToColor("#B9C9C7", 1.0f);
-    const Color ACCENT_DARK_COLOR = hexToColor("#82858A", 1.0f);
+	
+	const Color GRAY_0_COLOR_100 = hexToColor("#5A6164", 1.0f);
+	
+	const Color GRAY_1_COLOR_100 = hexToColor("#82858A", 1.0f);
+	
+	const Color GRAY_2_COLOR_100 = hexToColor("#9A9DA2", 1.0f);
+	
+	const Color GRAY_3_COLOR_100 = hexToColor("#B9C9C7", 1.0f);
+    const Color GRAY_3_COLOR_80 = hexToColor("#B9C9C7", 0.8f);
+	
+    const Color GRAY_4_COLOR_100 = hexToColor("#E4F4F4", 1.0f);
+    const Color GRAY_4_COLOR_80 = hexToColor("#E4F4F4", 0.8f);
+    const Color GRAY_4_COLOR_40 = hexToColor("#E4F4F4", 0.4f);
 
     int centerX = offsetX + scaledWidth / 2; // new top left for 16:9 (0,0)
     int centerY = offsetY + scaledHeight / 2;
@@ -33,22 +41,25 @@ MenuAction MenuScreen::render(int screenWidth, int screenHeight, int offsetX, in
         false
     );
 
-    DrawRectangle(offsetX + SCL(40), offsetY + SCL(20), SCL(726), SCL(4), ACCENT_LIGHT_COLOR_80); // top accent bar
+    DrawRectangle(offsetX + SCL(40), offsetY + SCL(20), SCL(726), SCL(4), GRAY_4_COLOR_80); // top accent bar
     DrawTriangle(
-        Vector2{(float)offsetX + SCL(740), (float)offsetY + SCL(24)},      
-        Vector2{(float)offsetX + SCL(766), (float)offsetY + SCL(50)}, 
-        Vector2{(float)offsetX + SCL(766), (float)offsetY + SCL(24)},         
-        ACCENT_LIGHT_COLOR_80
+        Vector2{(float)offsetX + SCL(740), (float)offsetY + SCL(24)},
+        Vector2{(float)offsetX + SCL(766), (float)offsetY + SCL(50)},
+        Vector2{(float)offsetX + SCL(766), (float)offsetY + SCL(24)},
+        GRAY_4_COLOR_80
     );
     UIComponents::drawGradientRect(
         gradientShader, 
         Rectangle{ (float)offsetX + SCL(766), (float)offsetY + SCL(20), (float)SCL(1114), (float)SCL(30) },
-        ACCENT_LIGHT_COLOR_80,
-        ACCENT_LIGHT_COLOR_40, 
+        GRAY_4_COLOR_80,
+        GRAY_4_COLOR_40, 
         false
     );
+	DrawRectangle(offsetX + SCL(1527), offsetY + SCL(30), SCL(100), SCL(10), GRAY_3_COLOR_80); //top accent bar details
+	DrawRectangle(offsetX + SCL(1647), offsetY + SCL(30), SCL(100), SCL(10), PRIMARY_COLOR_100);
+	DrawRectangle(offsetX + SCL(1767), offsetY + SCL(30), SCL(100), SCL(10), GRAY_3_COLOR_80);
 
-    DrawRectangle(offsetX + SCL(40), offsetY + SCL(40), SCL(10), SCL(200), ACCENT_LIGHT_COLOR_100); // title gradient vertical start
+    DrawRectangle(offsetX + SCL(40), offsetY + SCL(40), SCL(10), SCL(200), GRAY_4_COLOR_100); // title gradient vertical start
     UIComponents::drawGradientRect( // title gradient backing
         gradientShader, 
         Rectangle{ (float)offsetX + SCL(50), (float)offsetY + SCL(40), (float)SCL(600), (float)SCL(200) },
@@ -56,7 +67,51 @@ MenuAction MenuScreen::render(int screenWidth, int screenHeight, int offsetX, in
         hexToColor("#16846B", 0.0f), 
         false
     );
-
+	
+	DrawRectangle(offsetX + SCL(757), offsetY + SCL(109), SCL(200), SCL(2), GRAY_2_COLOR_100); // stat panel accent line
+	DrawLineEx(Vector2{offsetX + SCL(956), offsetY + SCL(110)}, Vector2{offsetX + SCL(1018), offsetY + SCL(170)}, 3.5f, GRAY_2_COLOR_100);
+	DrawRectangle(offsetX + SCL(1017), offsetY + SCL(169), SCL(800), SCL(2), GRAY_2_COLOR_100);
+	
+	DrawRectangle(offsetX + SCL(761), offsetY + SCL(219), SCL(4), SCL(4), GRAY_3_COLOR_80); // stat panel accent
+	DrawRectangle(offsetX + SCL(780), offsetY + SCL(219), SCL(213), SCL(4), GRAY_3_COLOR_80);
+	
+	for (int i{}; i < 5; ++i) {
+		DrawRectangle(offsetX + SCL(20), offsetY + SCL(422 + i * 100), SCL(300), SCL(2), GRAY_1_COLOR_100); //selection column 1 separator bars
+		DrawTriangle(
+			Vector2{(float)offsetX + SCL(67), (float)offsetY + SCL(424 + i * 100)},
+			Vector2{(float)offsetX + SCL(67), (float)offsetY + SCL(428 + i * 100)},
+			Vector2{(float)offsetX + SCL(71), (float)offsetY + SCL(424 + i * 100)},
+			GRAY_1_COLOR_100
+		);
+		DrawRectangle(offsetX + SCL(20), offsetY + SCL(424 + i * 100), SCL(47), SCL(4), GRAY_1_COLOR_100);
+		
+		DrawRectangle(offsetX + SCL(10), offsetY + SCL(434 + i * 100), SCL(4), SCL(4), GRAY_3_COLOR_100); //selection column 1 accent
+	}
+	
+	DrawRectangle(offsetX + SCL(442), offsetY + SCL(336), SCL(360), SCL(2), GRAY_1_COLOR_100); //selection column 2 upper border
+	DrawTriangle(
+        Vector2{(float)offsetX + SCL(742), (float)offsetY + SCL(336)},
+        Vector2{(float)offsetX + SCL(746), (float)offsetY + SCL(336)},
+        Vector2{(float)offsetX + SCL(746), (float)offsetY + SCL(332)},
+        GRAY_1_COLOR_100
+    );
+	DrawRectangle(offsetX + SCL(746), offsetY + SCL(332), SCL(56), SCL(4), GRAY_1_COLOR_100);
+	
+	DrawRectangle(offsetX + SCL(842), offsetY + SCL(336), SCL(1038), SCL(2), GRAY_1_COLOR_100); //details pane upper border
+	DrawTriangle(
+        Vector2{(float)offsetX + SCL(1820), (float)offsetY + SCL(336)},
+        Vector2{(float)offsetX + SCL(1824), (float)offsetY + SCL(336)},
+        Vector2{(float)offsetX + SCL(1824), (float)offsetY + SCL(332)},
+        GRAY_1_COLOR_100
+    );
+	DrawRectangle(offsetX + SCL(1824), offsetY + SCL(332), SCL(56), SCL(4), GRAY_1_COLOR_100);
+	
+	DrawRectangle(offsetX + SCL(842), offsetY + SCL(910), SCL(1038), SCL(2), GRAY_1_COLOR_100); //details pane lower border
+	
+	DrawRectangle(offsetX + SCL(10), offsetY + SCL(1068), SCL(100), SCL(6), GRAY_0_COLOR_100); //bottom left accent
+	DrawRectangle(offsetX + SCL(140), offsetY + SCL(1068), SCL(100), SCL(6), GRAY_4_COLOR_100);
+	DrawRectangle(offsetX + SCL(270), offsetY + SCL(1068), SCL(100), SCL(6), GRAY_0_COLOR_100);
+	DrawRectangle(offsetX + SCL(400), offsetY + SCL(1068), SCL(100), SCL(6), GRAY_0_COLOR_100);
     
     Vector2 mousePos = input.getMousePosition();
     bool isClicked = input.isMousePressed(MOUSE_LEFT_BUTTON);
@@ -64,11 +119,11 @@ MenuAction MenuScreen::render(int screenWidth, int screenHeight, int offsetX, in
     int playButtonY = centerY - SCL(20);
     int exitButtonY = centerY + SCL(40);
 
-    if (UIComponents::drawPlayButton(centerX, playButtonY, mousePos, isClicked, PRIMARY_COLOR_100, ACCENT_LIGHT_COLOR_80, BACKGROUND_COLOR, aspectScale)) {
+    if (UIComponents::drawPlayButton(centerX, playButtonY, mousePos, isClicked, PRIMARY_COLOR_100, GRAY_4_COLOR_80, BACKGROUND_COLOR, aspectScale)) {
         return MenuAction::START_3D;
     }
     
-    if (UIComponents::drawExitButton(centerX, exitButtonY, mousePos, isClicked, ACCENT_DARK_COLOR, ACCENT_MEDIUM_COLOR, ACCENT_LIGHT_COLOR_80, aspectScale)) {
+    if (UIComponents::drawExitButton(centerX, exitButtonY, mousePos, isClicked, GRAY_2_COLOR_100, GRAY_3_COLOR_80, GRAY_4_COLOR_80, aspectScale)) {
         return MenuAction::EXIT_GAME;
     }
 
