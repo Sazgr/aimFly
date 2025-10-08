581118aef9665b0fd7b8737eea828b44fc264553
Sazgr <kyleyzhang@gmail.com>
2025-10-08 15:47:55 -0400

    Cleanup

--- uicomponents.cpp
+++ uicomponents.cpp
@@ -89,34 +89,3 @@ bool Button::draw(int offsetX, int offsetY, float aspectScale, Vector2 mousePos,
 	#undef SCL
 	return isHovered && mouseClicked; // conditions for click
 }
-/*bool UIComponents::drawButtonCore(const char* text, int centerX, int centerY, int width, int height, Vector2 mousePos, bool isClicked, Color normalColor, Color hoverColor, Color textColor, float aspectScale) {
-    int scaledWidth = (int)(width * aspectScale);
-    int scaledHeight = (int)(height * aspectScale);
-    int scaledFontSize = (int)(20 * aspectScale);
-
-    Rectangle rect = { (float)centerX - scaledWidth/2, (float)centerY - scaledHeight/2, (float)scaledWidth, (float)scaledHeight };
-    bool isHovered = isPointInRect(mousePos, rect);
-    
-    DrawRectangleRec(rect, isHovered ? hoverColor : normalColor);
-
-    int textWidth = MeasureText(text, scaledFontSize);
-    DrawText(text, centerX - textWidth / 2, centerY - scaledFontSize / 2, scaledFontSize, textColor);
-
-    return isHovered && isClicked;
-}*/
-
-/*bool UIComponents::drawPlayButton(int centerX, int centerY, Vector2 mousePos, bool isClicked, Color normalColor, Color hoverColor, Color textColor, float aspectScale) {
-    //const int width = 200;
-    //const int height = 50;
-    //return drawButtonCore("START PRACTICE", centerX, centerY, width, height, mousePos, isClicked, normalColor, hoverColor, textColor, aspectScale);
-}
-
-bool UIComponents::drawExitButton(int centerX, int centerY, Vector2 mousePos, bool isClicked, Color normalColor, Color hoverColor, Color textColor, float aspectScale) {
-    //const int width = 200;
-    //const int height = 50;
-    //return drawButtonCore("EXIT", centerX, centerY, width, height, mousePos, isClicked, normalColor, hoverColor, textColor, aspectScale);
-}
-
-bool UIComponents::drawButton(const char* text, int centerX, int centerY, int width, int height, Vector2 mousePos, bool isClicked, Color normalColor, Color hoverColor, Color textColor, float aspectScale) {
-    //return drawButtonCore(text, centerX, centerY, width, height, mousePos, isClicked, normalColor, hoverColor, textColor, aspectScale);
-}*/
