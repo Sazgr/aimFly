37b7e8ad9805c41b334f4577edf7cb1f4618f59f
Sazgr <kyleyzhang@gmail.com>
2025-10-12 16:14:09 -0400

    Place function definitions in cpp file

--- target.cpp
+++ target.cpp
@@ -1,6 +1,48 @@
 #include "hitinfo.h"
 #include "target.h"
 
+Target::Target(TargetType t, Vector3 pos, float sz) {
+	type = t;
+	hitType = MISS;
+	position = pos;
+	size = sz;
+	if (type == TargetType::SPHERE) {
+		Mesh mesh = GenMeshSphere(size, 32, 32);
+		models.emplace_back(LoadModelFromMesh(mesh));
+	} else if (t == TargetType::BODY) {
+		Mesh mesh = GenMeshSphere(size, 32, 32);
+		models.emplace_back(LoadModelFromMesh(mesh));
+		mesh = GenMeshSphere(1.5 * size, 32, 32);
+		models.emplace_back(LoadModelFromMesh(mesh));
+		mesh = GenMeshCylinder(1.5 * size, 2.5 * size, 32);
+		models.emplace_back(LoadModelFromMesh(mesh));
+		health = 4;
+	}
+}
+
+Target::~Target() {
+	for (int i{}; i < models.size(); ++i) {
+		UnloadModel(models[i]);
+	}
+}
+
+void Target::addShader(Shader& shader) {
+	for (int i{}; i < models.size(); ++i) {
+		this->models[i].materials[0].shader = shader;
+	}
+}
+
+void Target::draw() {
+	if (type == TargetType::SPHERE) {
+		DrawModel(models[0], position, 1.0f, SKYBLUE);
+	} else if (type == TargetType::BODY) {
+		DrawModel(models[0], position, 1.0f, hitType == HEADSHOT ? YELLOW : SKYBLUE);
+		DrawModel(models[1], Vector3Add(position, Vector3{0.0f, - 2 * size, 0.0f}), 1.0f, hitType == BODYSHOT ? YELLOW : SKYBLUE);
+		DrawModel(models[2], Vector3Add(position, Vector3{0.0f, - 4.5f * size, 0.0f}), 1.0f, hitType == BODYSHOT ? YELLOW : SKYBLUE);
+		DrawModel(models[1], Vector3Add(position, Vector3{0.0f, - 4.5f * size, 0.0f}), 1.0f, hitType == BODYSHOT ? YELLOW : SKYBLUE);
+	}
+}
+
 HitInfo Target::hitscan(Vector3 camPos, Vector3 forward) {
 	Ray ray{camPos, forward};		
 	if (type == TargetType::SPHERE) {
